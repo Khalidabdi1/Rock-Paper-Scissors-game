febf5a0913447e32ce827c7aadfddcd910e61eac
Khalid Abdi <xamza2009@gmail.com>
2025-07-26 22:31:05 +0300

    edit result card

--- src/component/Result..js
+++ src/component/Result..js
@@ -3,8 +3,29 @@
 
 export default function Result(){
     return (
-        <>
-        <h1 style={{color:"white"}}>dd</h1>
-        </>
+      <div style={{display:"flex",alignItems:"center",justifyContent:"space-around"}}>
+        <div className="Result-one">
+              <h3 className="Result-title-one" > 
+                    You Result :
+                </h3>
+            <div className="Result-one-inside">
+              
+                <h3 style={{textAlign:"center"}}>
+                    0
+                </h3>
+            </div>
+         </div>
+
+        <div  className="Result-one">
+                <h3 className="Result-title-one" > 
+                     computer Result:
+                </h3>
+             <div className="Result-one-inside">
+                <h3 style={{textAlign:"center"}}>
+                    0
+                </h3>
+            </div>
+        </div>
+      </div>
     )
 }
--- src/component/photo.js
+++ src/component/photo.js
@@ -2,21 +2,23 @@
 
 export default function Photo(){
     return (
-        <div style={{display:"flex" ,backgroundColor:"red",width:"100%",height:"400px"}}>
+        <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+        <div className="card-photo" style={{display:"flex" ,width:"90%",height:"400px",justifyContent:"space-between"}}>
 
             <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
-            <img src="/paper.png" alt="paper" style={{width:"50%"}}></img>
+            <img src="/paper.png" alt="paper" style={{width:"300px",height:"300px"}}></img>
             <h3>You</h3>
             </div>
 
             <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
-            <img src="/paper.png" alt="paper" style={{width:"50%"}}></img>
+            <img src="/paper.png" alt="paper" style={{width:"300px",height:"300px"}}></img>
              <h3>computer</h3>
 
             </div>
 
       
 
+        </div>
         </div>
     )
 }
