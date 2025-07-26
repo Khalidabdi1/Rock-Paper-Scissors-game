1cdaddcbf8cebeccc8673b5724039eed0f8d3618
Khalid Abdi <xamza2009@gmail.com>
2025-07-26 21:30:54 +0300

    project start

---
+++ src/component/Result..js
@@ -0,0 +1,10 @@
+
+
+
+export default function Result(){
+    return (
+        <>
+        <h1 style={{color:"white"}}>dd</h1>
+        </>
+    )
+}
---
+++ src/component/game.js
@@ -0,0 +1,15 @@
+import Result from "./Result."
+export default function Game(){
+    return(
+
+    <>
+   
+   <div>
+    <Result/>
+    
+   </div>
+
+    </>
+
+    )
+}
